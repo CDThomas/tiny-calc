6b6c5815fd05f246b34fbdbf83e0e6f8e5bb9900
Drew Thomas <cdthomas.92@gmail.com>
2022-08-15 07:04:43 +1000

    Refactor parser

--- tiny-calc-core/src/parser.rs
+++ tiny-calc-core/src/parser.rs
@@ -1,33 +1,18 @@
-// use nom::branch::alt;
-// use nom::character::complete::{char, i64, space0};
-// use nom::combinator::{eof, map};
-// use nom::sequence::tuple;
-// use nom::IResult;
-
 use nom::branch::alt;
-use nom::character::complete::{char, digit1, space0};
-use nom::combinator::map;
+use nom::character::complete::{char, i64, space0};
+use nom::combinator::{eof, map};
 use nom::multi::many0;
 use nom::sequence::{delimited, tuple};
 use nom::IResult;
-// use std::str::FromStr;
 
 use crate::ast::*;
 
 pub fn parse(input: &str) -> Result<Calculation, nom::Err<nom::error::Error<&str>>> {
-    // TODO: parse fully
-    parse_math_expr(input).map(|(_rem, expr)| Calculation {
+    fully(parse_math_expr, input).map(|(_rem, expr)| Calculation {
         expr,
         formatter: None,
     })
 }
-
-// fn parse_basic_expr(input: &str) -> IResult<&str, Calculation> {
-//     parse_math_expr(input).map(|expr| Calculation {
-//         expr,
-//         formatter: None,
-//     })
-// }
 
 fn parse_parens(input: &str) -> IResult<&str, Expr> {
     delimited(
@@ -63,18 +48,9 @@ fn parse_expr(expr: Expr, rem: Vec<(char, Expr)>) -> Expr {
     rem.into_iter().fold(expr, |acc, val| parse_op(val, acc))
 }
 
-fn parse_op(tup: (char, Expr), expr1: Expr) -> Expr {
-    let (op, expr2) = tup;
-    // match op {
-    //     '+' => EAdd(Box::new(expr1), Box::new(expr2)),
-    //     '-' => ESub(Box::new(expr1), Box::new(expr2)),
-    //     '*' => EMul(Box::new(expr1), Box::new(expr2)),
-    //     '/' => EDiv(Box::new(expr1), Box::new(expr2)),
-    //     '^' => EExp(Box::new(expr1), Box::new(expr2)),
-    //     _ => panic!("Unknown Operation"),
-    // }
-
+fn parse_op((op, rhs): (char, Expr), lhs: Expr) -> Expr {
     let op = match op {
+        // TODO: div and exp
         '+' => Op::Add,
         '-' => Op::Sub,
         '*' => Op::Mul,
@@ -82,95 +58,21 @@ fn parse_op(tup: (char, Expr), expr1: Expr) -> Expr {
     };
 
     Expr::BinaryOp(BinaryOp {
-        lhs: Box::new(expr1),
+        lhs: Box::new(lhs),
         op,
-        rhs: Box::new(expr2),
+        rhs: Box::new(rhs),
     })
 }
 
-fn parse_enum(parsed_num: &str) -> Expr {
-    let num = i64::from_str_radix(parsed_num, 10).unwrap();
-    Expr::Integer(Integer { value: num })
-}
-
 fn parse_number(input: &str) -> IResult<&str, Expr> {
-    map(delimited(space0, digit1, space0), parse_enum)(input)
+    map(delimited(space0, i64, space0), |value| {
+        Expr::Integer(Integer { value })
+    })(input)
 }
 
-// fn integer(input: &str) -> IResult<&str, Expr> {
-//     let (input, value) = i64(input)?;
-//     Ok((input, Expr::Integer(Integer { value })))
-// }
-
-// fn term(input: &str) -> IResult<&str, Expr> {
-//     alt((
-//         map(
-//             tuple((integer, space0, alt((char('+'), char('-'))), space0, term)),
-//             // tuple((integer, space0, char('*'), space0, term)),
-//             |(lhs, _, op, _, rhs)| {
-//                 let op = match op {
-//                     '+' => Op::Add,
-//                     '-' => Op::Sub,
-//                     '*' => Op::Mul,
-//                     _ => unreachable!(),
-//                 };
-
-//                 Expr::BinaryOp(BinaryOp {
-//                     lhs: Box::new(lhs),
-//                     op,
-//                     rhs: Box::new(rhs),
-//                 })
-//             },
-//         ),
-//         map(integer, |i| i),
-//     ))(input)
-// }
-
-// fn expr(input: &str) -> IResult<&str, Expr> {
-//     alt((
-//         map(
-//             // tuple((term, space0, alt((char('+'), char('-'))), space0, expr)),
-//             tuple((term, space0, char('*'), space0, expr)),
-//             |(lhs, _, op, _, rhs)| {
-//                 let op = match op {
-//                     '+' => Op::Add,
-//                     '-' => Op::Sub,
-//                     '*' => Op::Mul,
-//                     _ => unreachable!(),
-//                 };
-
-//                 Expr::BinaryOp(BinaryOp {
-//                     lhs: Box::new(lhs),
-//                     op,
-//                     rhs: Box::new(rhs),
-//                 })
-//             },
-//         ),
-//         // map(
-//         //     tuple((term, space0, char('-'), space0, expr)),
-//         //     |(lhs, _, _, _, rhs)| {
-//         //         Expr::BinaryOp(BinaryOp {
-//         //             lhs: Box::new(lhs),
-//         //             op: Op::Sub,
-//         //             rhs: Box::new(rhs),
-//         //         })
-//         //     },
-//         // ),
-//         map(term, |t| t),
-//     ))(input)
-// }
-
-// fn fully<A>(parser: impl FnMut(&str) -> IResult<&str, A>, input: &str) -> IResult<&str, A> {
-//     map(tuple((space0, parser, eof)), |(_, expr, _)| expr)(input)
-// }
-
-// pub fn parse(input: &str) -> Result<Calculation, nom::Err<nom::error::Error<&str>>> {
-//     fully(expr, input).map(|(_, expr)| Calculation {
-//         expr,
-//         formatter: None,
-//     })
-// }
-
+fn fully<A>(parser: impl FnMut(&str) -> IResult<&str, A>, input: &str) -> IResult<&str, A> {
+    map(tuple((space0, parser, eof)), |(_, expr, _)| expr)(input)
+}
 #[cfg(test)]
 mod test {
     use super::*;
